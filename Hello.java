62c1cfc85f5336342b8702733b70fd0ffe463439
yashshuklaa001 <yashshuklaa001@gmail.com>
2026-02-16 10:51:27 +0530

    New Change

--- Hello.java
+++ Hello.java
@@ -1,4 +1,6 @@
-public static void main(String[] args) {
-    System.out.println("Hello, World!");
-    system.out.println("Welcome to Java programming.");
+class Hello{
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Java programming.");
+    }
 }
